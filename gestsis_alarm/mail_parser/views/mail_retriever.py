4ff0246f27a02ba187347d554d1207295c03002a
Alexia Montavon <alexia@montavon.xyz>
2022-06-09 14:59:29 +0200

    feat(#1): Add base view for mail retrieving

---
+++ gestsis_alarm/mail_parser/views/mail_retriever.py
@@ -0,0 +1,76 @@
+import imaplib
+import email
+from pathlib import Path
+import uuid
+
+
+class MailRetriever:
+    _imap_connection = None
+    _mail_whitelist = []
+
+    def __init__(self, mail_server, port, username, password, mail_whitelist):
+
+        self._imap_connection = imaplib.IMAP4_SSL(mail_server, port)
+        self._imap_connection.login(username, password)
+
+        self._mail_whitelist = mail_whitelist
+
+    def check_for_new_messages(self, delete_on_read=False):
+        new_attachments = []
+
+        messages_list = self._retrieve_messages()
+        for mail in messages_list:
+            self._save_attachment(mail)
+
+        return new_attachments
+
+    def _retrieve_messages(self):
+        # Retrieve ALL emails in the INBOX
+        status, messages = self._imap_connection.select("INBOX")
+        stat, data = self._imap_connection.search(None, 'ALL')
+
+        mail_list = []
+
+        for mail_id in data[0].split():
+            content = self._fetch_message(mail_id)
+            if content is not None:
+                mail_list.append(content)
+
+        return mail_list
+
+    def _fetch_message(self, mail_id):
+        status, data = self._imap_connection.fetch(mail_id, '(RFC822)')
+        response_part = data[0][1]
+
+        message = email.message_from_bytes(response_part)
+
+        if "from" not in message or "subject" not in message:
+            return None  # Shouldn't happen, but better to be sure
+
+        # Discard message if there is no attachment for us
+        if message.get_content_maintype() != 'multipart':
+            return None
+
+        mail_from = message['from']
+        mail_subject = message['subject']
+
+        # Check if mail address is in the whitelist
+        if email.utils.parseaddr(mail_from)[1] not in self._mail_whitelist:
+            return None
+
+        return message
+
+    @staticmethod
+    def _save_attachment(msg):
+        filenames = []
+
+        for part in msg.walk():
+            # Check if we have a PDF (The extension check is here to be extra sure)
+            if part.get_content_type() == "application/pdf" and Path(part.get_filename()).suffix == ".pdf":
+                filename = "../pdf/" + str(uuid.uuid4()) + "_" + part.get_filename()
+                with open(filename, 'wb') as fp:
+                    fp.write(part.get_payload(decode=True))
+
+                filenames.append(filename)
+
+        return filenames
