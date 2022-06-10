9cd1cb4cfe2238b66e9f39dd3f6f911cd9851cc3
Alexia Montavon <alexia@montavon.xyz>
2022-06-10 15:51:56 +0200

    chore(#1): Add storage folder for pdf file

--- gestsis_alarm/gestsis_alarm/settings.py
+++ gestsis_alarm/gestsis_alarm/settings.py
@@ -114,11 +114,14 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Storage files
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'storage')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
