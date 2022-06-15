244f5978811875e9a8be76851e005b32d5e0ba93
Alexia Montavon <alexia@montavon.xyz>
2022-06-15 10:53:27 +0200

    fix(#1): retrieve_mail crashed when the environment variable where not set

--- gestsis_alarm/mail_parser/management/commands/retrieve_mail.py
+++ gestsis_alarm/mail_parser/management/commands/retrieve_mail.py
@@ -10,20 +10,23 @@ class Command(BaseCommand):
     _required_options = ["server", "port", "username", "password", "whitelisted_mails"]
 
     def add_arguments(self, parser):
-        parser.add_argument('--server', type=str, default=os.environ["GESTSIS_ALARM_MAIL_SERVER"],
+        parser.add_argument('--server', type=str, default=os.environ.get("GESTSIS_ALARM_MAIL_SERVER", None),
                             help="Url of the mail server")
-        parser.add_argument('--port', type=int, default=os.environ["GESTSIS_ALARM_MAIL_PORT"],
+        parser.add_argument('--port', type=int, default=os.environ.get("GESTSIS_ALARM_MAIL_PORT", None),
                             help="Port of the mail server")
-        parser.add_argument('--username', type=str, default=os.environ["GESTSIS_ALARM_MAIL_USERNAME"],
+        parser.add_argument('--username', type=str, default=os.environ.get("GESTSIS_ALARM_MAIL_USERNAME", None),
                             help="Can be a username or your email address depending on your email provider")
-        parser.add_argument('--password', type=str, default=os.environ["GESTSIS_ALARM_MAIL_PASSWORD"])
-        parser.add_argument('--whitelisted-mails', nargs='+', type=str, default=os.environ["GESTSIS_ALARM_MAIL_WHITELIST"].split(","))
+        parser.add_argument('--password', type=str, default=os.environ.get("GESTSIS_ALARM_MAIL_PASSWORD", None))
+        parser.add_argument('--whitelisted-mails', nargs='+', type=str, default=os.environ.get("GESTSIS_ALARM_MAIL_WHITELIST", None))
 
     def handle(self, *args, **options):
 
         for option in self._required_options:
             if not options[option]:
                 raise ValueError("'{}' cannot be empty or None ! Specify it with the command line or in the .env file !".format(option))
+
+        if type(options["whitelisted_mails"]) == str:
+            options["whitelisted_mails"] = options["whitelisted_mails"].strip(",")
 
         mc = MailRetriever(options["server"], options["port"], options["username"], options["password"], options["whitelisted_mails"])
         pdf_downloaded = mc.check_for_new_messages()
