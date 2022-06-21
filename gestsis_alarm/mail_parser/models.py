83bb4abe72b46c98063dc42d02f63319c63a0966
Alexia Montavon <alexia@montavon.xyz>
2022-06-21 10:31:47 +0200

    feat(#2): Add models

--- gestsis_alarm/mail_parser/models.py
+++ gestsis_alarm/mail_parser/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Sis(models.Model):
+    name = models.CharField()
+    gestsis_name = models.CharField()
+
+
+class Alarm(models.Model):
+    sis_id = models.ForeignKey(Sis, on_delete=models.CASCADE)
+    address = models.CharField()
+    location_wgs84 = models.CharField()
+    location_lv95 = models.CharField(max_length=20)
+    type = models.CharField()
+    has_been_read = models.BooleanField(default=False)
+
+
+class Firefighter(models.Model):  # Dans le cas présent, le sapeur est lié à l'alarme...
+    alarm_id = models.ForeignKey(Alarm, on_delete=models.CASCADE)
+    group = models.CharField()
+    fullname = models.CharField()
+    phone = models.CharField(max_length=50)
+
+
+class File(models.Model):
+    alarm_id = models.ForeignKey(Alarm, on_delete=models.CASCADE)
+    filename = models.CharField()
+
