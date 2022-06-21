dce442a9cc47d91be157c272bbdf1e1c78a9194d
Alexia Montavon <alexia@montavon.xyz>
2022-06-21 11:44:43 +0200

    fix(#2): Add max_length to CharField on models

--- gestsis_alarm/mail_parser/models.py
+++ gestsis_alarm/mail_parser/models.py
@@ -2,27 +2,27 @@ from django.db import models
 
 
 class Sis(models.Model):
-    name = models.CharField()
-    gestsis_name = models.CharField()
+    name = models.CharField(max_length=255)
+    gestsis_name = models.CharField(max_length=255)
 
 
 class Alarm(models.Model):
     sis_id = models.ForeignKey(Sis, on_delete=models.CASCADE)
-    address = models.CharField()
-    location_wgs84 = models.CharField()
+    address = models.CharField(max_length=255)
+    location_wgs84 = models.CharField(max_length=255)
     location_lv95 = models.CharField(max_length=20)
-    type = models.CharField()
+    type = models.CharField(max_length=100)
     has_been_read = models.BooleanField(default=False)
 
 
 class Firefighter(models.Model):  # Dans le cas présent, le sapeur est lié à l'alarme...
     alarm_id = models.ForeignKey(Alarm, on_delete=models.CASCADE)
-    group = models.CharField()
-    fullname = models.CharField()
+    group = models.CharField(max_length=255)
+    fullname = models.CharField(max_length=255)
     phone = models.CharField(max_length=50)
 
 
 class File(models.Model):
     alarm_id = models.ForeignKey(Alarm, on_delete=models.CASCADE)
-    filename = models.CharField()
+    filename = models.CharField(max_length=255)
 
