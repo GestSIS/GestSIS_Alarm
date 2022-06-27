33dabe5aa3f194ed251991e561cc577b2af2d54e
Alexia Montavon <alexia@montavon.xyz>
2022-06-27 08:27:51 +0200

    chore(#2): Move PDF Data to a new file

--- gestsis_alarm/mail_parser/tasks/pdf_extraction.py
+++ gestsis_alarm/mail_parser/tasks/pdf_extraction.py
@@ -3,6 +3,7 @@ from pdfminer.layout import LAParams, LTTextContainer, LTTextLine
 from enum import Enum
 import re
 from collections import deque
+from .utils.pdf_data import PDFData
 
 
 class PDFExtractionException(Exception):
@@ -20,99 +21,6 @@ class ReadingMode(Enum):
     SEARCH_SIS = 1
     SEARCH_STATS = 2
     SEARCH_FIREFIGHTER = 3
-
-
-class PDFData:
-    """
-    Sort of a container to store the information extracted from the PDF
-    """
-    alarm_type = None
-    lv95_coordinate = None
-    event_address = None
-    firefighter_coming = {}
-    _current_group = None
-    _current_sis = None
-
-    def add_message_info(self, alarm_type, event_address, lv95_coordinate):
-        self.alarm_type = alarm_type
-        self.lv95_coordinate = lv95_coordinate
-        self.event_address = event_address
-
-    def add_firefighter_to_current_group(self, firefighter: str, phone: str):
-        """
-        Add Firefighter to the current group in the current SIS
-        :param
-            firefighter: str
-              The name of the firefighter
-        :param
-            phone: str
-              The phone number(s) of the firefighter, the string can contain multiple phone number
-        :return True if the firefighter has been added, otherwise False
-        """
-        if not self._current_sis or not self._current_group:
-            return False
-
-        f = {
-            "name": firefighter,
-            "phone": phone
-        }
-
-        if f in self.firefighter_coming[self._current_sis][self._current_group]:
-            return False
-
-        self.firefighter_coming[self._current_sis][self._current_group].append(f)
-
-        return True
-
-    def add_sis(self, name: str):
-        """
-        Add SIS to the dictionary except if it's already in it.
-        The variable current_sis is ALWAYS MODIFIED by each call, even if the SIS is already in the dictionary
-        :param
-            name: str
-              Name of the SIS
-        :return: True if the SIS has been added, otherwise False
-        """
-        self._current_sis = name
-
-        if name in self.firefighter_coming:
-            return False
-
-        self.firefighter_coming[name] = {}
-
-        return True
-
-    def add_group(self, name: str):
-        """
-        Add a group to the dictionary except if it's already in it.
-        The variable current_group is modified ONLY if the group is not in the current SIS.
-        :param
-            name: str
-              Name of the group
-        :return: True if the group has been added, otherwise False
-        """
-
-        if not self._current_sis or name in self.firefighter_coming[self._current_sis]:
-            return False
-
-        self.firefighter_coming[self._current_sis][name] = []
-        self._current_group = name
-
-        return True
-
-    def get_current_group(self):
-        if not self._current_sis or not self._current_group:
-            return None
-
-        return self.firefighter_coming[self._current_sis][self._current_group]
-
-    def get_current_group_name(self):
-        return "{}, {}".format(self._current_sis, self._current_group)
-
-    def __str__(self):
-        return "Alarm Type: {}\nLV95: {}\nAddress: {}\nFirefighters: {}".format(self.alarm_type, self.lv95_coordinate,
-                                                                                self.event_address,
-                                                                                self.firefighter_coming)
 
 
 class PDFExtractor:
