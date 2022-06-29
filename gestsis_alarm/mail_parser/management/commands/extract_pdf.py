b55923cc8094232601fd64dd98a84f45fedd30a2
Alexia Montavon <alexia@montavon.xyz>
2022-06-29 18:22:31 +0200

    feat(#2): Retrieve SIS from the database

--- gestsis_alarm/mail_parser/management/commands/extract_pdf.py
+++ gestsis_alarm/mail_parser/management/commands/extract_pdf.py
@@ -4,6 +4,8 @@ from django.core.management import BaseCommand
 import os.path
 from django.conf import settings
 from ...tasks.pdf_extraction import PDFExtractor, PDFExtractionException
+from ...models import Alarm, File, Firefighter, Sis
+from ...utils.lv95_converter import convert_lv95_to_wgs84
 
 
 class Command(BaseCommand):
@@ -28,12 +30,7 @@ class Command(BaseCommand):
         if not os.path.isabs(options["pdf_file"]):
             options["pdf_file"] = os.path.join(settings.MEDIA_ROOT, options["pdf_file"])
 
-        # The SIS will be retrieve from the database. This variable is just temporary and for test purpose
-        allowed_sis = [
-            "SIS Basse-Allaine", "SIS Val-Terbi", "SIS Vendline", "SIS Clos-du-Doubs", "SIS FM Centre", "SIS FM Ouest",
-            "SIS Haute-Sorne", "SIS Calabri", "SIS Baroche", "SIS 6/12", "SIS Haut-Plateau", "SIS Mont-Terri"
-        ]
-
+        allowed_sis = Sis.objects.values_list("name", flat=True)
         extractor = PDFExtractor(allowed_sis)
 
         try:
