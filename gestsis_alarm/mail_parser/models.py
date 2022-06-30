f8ec332affed4d995933d970ebc9cb8692808428
Alexia Montavon <alexia@montavon.xyz>
2022-06-30 15:00:21 +0200

    feat(#2): Make File unique in database

--- gestsis_alarm/mail_parser/models.py
+++ gestsis_alarm/mail_parser/models.py
@@ -46,7 +46,7 @@ class Alarm(models.Model):
 
 class File(models.Model):
     alarm = models.ForeignKey(Alarm, on_delete=models.CASCADE)
-    filename = models.CharField(max_length=255)
+    filename = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
         return self.filename
