b8e177cab041030f670a369bc53deb252be7855a
Alexia Montavon <alexia@montavon.xyz>
2022-06-30 17:40:22 +0200

    chore(#2): Move pdf extraction to pdfcommand class

--- gestsis_alarm/mail_parser/management/commands/extract_pdf.py
+++ gestsis_alarm/mail_parser/management/commands/extract_pdf.py
@@ -1,10 +1,7 @@
-import sys
-
 import os.path
 from django.conf import settings
 from ...tasks.pdf_extraction import PDFExtractor, PDFExtractionException
 from ...models import Sis
-from ...utils.lv95_converter import convert_lv95_to_wgs84
 from ._pdf_handling import PDFCommand
 
 
@@ -33,18 +30,4 @@ class Command(PDFCommand):
         allowed_sis = Sis.objects.values_list("name", flat=True)
         extractor = PDFExtractor(allowed_sis)
 
-        try:
-            data = extractor.extract_data(options["pdf_file"])
-        except PDFExtractionException as e:
-            self.stderr.write(self.style.ERROR("ERROR while parsing : {}".format(e.message), file=sys.stderr))
-            return
-
-        wgs84_coord = convert_lv95_to_wgs84(data.message.lv95_coordinate)
-
-        if wgs84_coord is None:
-            self.stderr.write(self.style.ERROR("ERROR while converting the coordinates ! (Given: {})".format(data.message.lv95_coordinate)))
-            return
-
-        self.stdout.write(self.style.SUCCESS("Successfully extracted the following data :"))
-        self.stdout.write(data)
-
+        self._handle_pdf(os.path.basename(options["pdf_file"]), options["pdf_file"], extractor)
