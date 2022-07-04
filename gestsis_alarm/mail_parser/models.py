2045acccfc575550156a1df238104176aad1fc0b
Alexia Montavon <alexia@montavon.xyz>
2022-07-04 17:51:37 +0200

    fix(#2): Change gestsis_name to gestsis_key

--- gestsis_alarm/mail_parser/models.py
+++ gestsis_alarm/mail_parser/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Sis(models.Model):
     name = models.CharField(max_length=255)
-    gestsis_name = models.CharField(max_length=255)
+    gestsis_key = models.CharField(max_length=10)
 
     def __str__(self):
         return self.name
