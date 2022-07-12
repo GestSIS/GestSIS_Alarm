c0c635e0c9437563ccd59f868a1dc0abe1b6bd51
Alexia Montavon <alexia@montavon.xyz>
2022-07-12 17:44:50 +0200

    feat(#9): Add Serializer for Firefighter

--- gestsis_alarm/admin_panel/serializers.py
+++ gestsis_alarm/admin_panel/serializers.py
@@ -1,11 +1,19 @@
 from rest_framework import serializers
-from mail_parser.models import Sis, Alarm
+from mail_parser.models import Firefighter, Sis, Alarm
 
 
 class SisSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Sis
         fields = ["id", "name", "gestsis_key"]
+
+
+class FirefighterSerializer(serializers.ModelSerializer):
+    sis = serializers.SlugRelatedField(slug_field="gestsis_key", read_only=True)
+
+    class Meta:
+        model = Firefighter
+        fields = ["fullname", "group_name", "group_number", "phone", "sis"]
 
 
 class AlarmSerializer(serializers.ModelSerializer):
