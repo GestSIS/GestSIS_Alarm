1a3007cff0ffac106193dd0810766b6a80a36888
Alexia Montavon <alexia@montavon.xyz>
2022-07-15 08:01:00 +0200

    feat: Replace secret key placeholder with init.py

--- init.py
+++ init.py
@@ -5,6 +5,18 @@ import os.path
 import shutil
 import fileinput
 
+
+def is_secret_key_invalid(line: str):
+    """
+    Check if a valid secret key is invalid on the line given.
+    Check if line is empty or if the line has the placeholder
+    :param line:
+    :return: True if the key is invalid, otherwise False
+    """
+    return (line.strip() == "GESTSIS_ALARM_SECRET_KEY=" or
+        line.strip().startswith("GESTSIS_ALARM_SECRET_KEY='-------- KEY HERE ---------'"))
+
+
 # Force replace of the SECRET_KEY if -f argument is given
 force_replace = (len(sys.argv) > 1 and sys.argv[1] == "-f")
 
@@ -27,7 +39,7 @@ has_token_been_replaced = False
 with fileinput.input(files=ENV_FILENAME, inplace=True) as f:
     for line in f:
         if line.strip().startswith("GESTSIS_ALARM_SECRET_KEY=") and \
-                (line.strip() == "GESTSIS_ALARM_SECRET_KEY=" or force_replace):
+                (is_secret_key_invalid(line) or force_replace):
             print("GESTSIS_ALARM_SECRET_KEY=\"{}\"".format(get_random_secret_key()))
             has_token_been_replaced = True
         else:
