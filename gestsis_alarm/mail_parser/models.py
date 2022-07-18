ce260ab12143cdf565045683b341e0cfa4b59793
Alexia Montavon <alexia@montavon.xyz>
2022-07-18 09:22:09 +0200

    feat: Make gestsis_key and name field unique in model

--- gestsis_alarm/mail_parser/models.py
+++ gestsis_alarm/mail_parser/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Sis(models.Model):
-    name = models.CharField(max_length=255)
-    gestsis_key = models.CharField(max_length=10)
+    name = models.CharField(max_length=255, unique=True)
+    gestsis_key = models.CharField(max_length=10, unique=True)
 
     def __str__(self):
         return self.name
