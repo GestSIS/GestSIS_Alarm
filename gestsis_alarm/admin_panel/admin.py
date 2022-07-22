4e25bf9e0ae703b68241a2e22ec7a580fd079265
Bastien Wermeille <bastien.wermeille@gmail.com>
2022-07-22 20:08:16 +0200

    Config auto deploy + admin pannel + sis test

--- gestsis_alarm/admin_panel/admin.py
+++ gestsis_alarm/admin_panel/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from mail_parser.models import Sis, Alarm, Firefighter
 
 # Register your models here.
+admin.site.register(Sis)
+admin.site.register(Alarm)
+admin.site.register(Firefighter)
