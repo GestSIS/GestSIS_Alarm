0d234234a7c8d14b2f2134706ecaa876146f6685
Bastien Wermeille <bastien.wermeille@gmail.com>
2023-09-13 22:47:48 +0200

    Ajout models au panel admin

--- admin_panel/admin.py
+++ admin_panel/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from mail_parser.models import Sis, Alarm, Firefighter
+from mail_parser.models import Sis, Alarm, Firefighter, Group, File
 
 # Register your models here.
 admin.site.register(Sis)
 admin.site.register(Alarm)
 admin.site.register(Firefighter)
+admin.site.register(Group)
+admin.site.register(File)
