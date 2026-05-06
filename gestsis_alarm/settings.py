618306dd1468c853794bc6f167082fd67f5565d3
Bastien Wermeille <bastien.wermeille@gmail.com>
2026-05-06 09:03:55 +0200

    Fix settings

--- gestsis_alarm/settings.py
+++ gestsis_alarm/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env(GESTSIS_ALARM_DEBUG_MODE=(bool, False))
 environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
-sentry_sdk.init(
-    dsn=env("SENTRY_DSN"),
-    # Add data like request headers and IP for users;
-    # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
-    send_default_pii=True,
-)
+if env("SENTRY_DSN"):
+    sentry_sdk.init(
+        dsn=env("SENTRY_DSN"),
+        # Add data like request headers and IP for users;
+        # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
+        send_default_pii=True,
+    )
 
 SECRET_KEY = env("GESTSIS_ALARM_SECRET_KEY")
 
